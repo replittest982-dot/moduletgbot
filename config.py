442ae9a24f208b4c344d48d8ec48deff993bdbb0
replittest982-dot <replittest982@gmail.com>
2025-11-29 11:40:31 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import pytz
+
+# --- AIOGRAM BOT ---
+BOT_TOKEN = "7868097991:AAFEVKips3QYTeXN9cLKg8eTOzYLKlt_S7A" 
+ADMIN_ID = 6256576302  # Ваш ID
+
+# --- TELETHON CLIENT ---
+API_ID = 37185453
+API_HASH = "7e40ac0357454dddbfbbfce511c9ddf1"
+
+# --- НАСТРОЙКИ ---
+SUPPORT_BOT_USERNAME = "suppor_tstatpro1bot"
+DB_NAME = 'bot_database.db'
+TIMEZONE_MSK = pytz.timezone('Europe/Moscow')
+RATE_LIMIT_TIME = 0.5
